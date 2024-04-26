7a79a071cdf3688102df4933e2fa130aeb7c99db
AmandineJouvenel <amandine.jouvenel@sorbonne-nouvelle.fr>
2024-04-26 17:22:59 +0200

    Correction backspaces + deletes dans identify_errors.py

--- creation_lexique.py
+++ creation_lexique.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 import spacy
+from tqdm import tqdm
+
 
 nlp = spacy.load("fr_core_news_sm")
 
@@ -11,20 +13,23 @@ def get_filenames(chemin_dossier: str) -> list[Path]:
 
 
 file_liste = get_filenames("TextesFinaux_txt")
-# print(file_liste)
-lexique = []
-for file in file_liste:
-    with open(file, 'r') as lecture_fichier:
-        lecture = lecture_fichier.readlines()
-        for line in lecture:
-            line = line.strip()
-            mots = nlp(line)
-            for mot in mots:
-                mot = mot.text.lower() #on recupp le text sinon probleme il recupère des attributs mots de spacy 
-                if mot not in lexique : 
-                    lexique.append(mot)
-                else : 
-                    pass
+#print(file_liste)
+def obtenir_lexique(file_liste):
+    lexique = []
+    for file in tqdm(file_liste, desc="Obtention du lexique") : 
+        with open(file, 'r') as lecture_fichier:
+            lecture = lecture_fichier.readlines()
+            for line in lecture:
+                line = line.strip()
+                mots = nlp(line)
+                for mot in mots:
+                    mot = mot.text.lower() #on recupp le text sinon probleme il recupère des attributs mots de spacy 
+                    if mot not in lexique : 
+                        lexique.append(mot)
+                    else : 
+                        pass
+    return lexique
 
-print(lexique)
+#obtenir_lexique(file_liste)
 
+# print(lexique) 
--- identify_errors.py
+++ identify_errors.py
@@ -1,9 +1,8 @@
-from datastructure import Ligne, Token, Erreur
+from datastructure import Ligne, Token, Difference
 import csv
 from typing import List
 from creation_lexique import obtenir_lexique, get_filenames
 import spacy
-from tqdm import tqdm
 
 nlp = spacy.load("fr_core_news_sm")
 
@@ -11,161 +10,52 @@ def ouverture_csv(file : str) -> List[Ligne]:
     """Ouvre un fichier csv et retourne une liste de lignes."""
     with open (file, 'r', encoding='utf-8') as f:
         reader = csv.reader(f, delimiter='\t') ## le delimiter de notre csv c'est des tabs
+        next(reader, None)
         liste_lignes = []
-        for row in tqdm(reader, desc="Lecture du fichier csv"):
-            line = Ligne(texte=row[16], tokens=[], categorie=row[15])
+        for row in reader:
+            line = Ligne(texte_complete=row[16], texte_simple = row[11], categorie=row[15], start_position=int(row[12]), end_position=int(row[13]), doc_length=int(row[14]), id=row[0], n_burst=int(row[3]))
             liste_lignes.append(line)
         return liste_lignes
 
-
-liste_lignes = ouverture_csv("csv_planification.csv")
+        # next(reader)
+        # lignes = [Ligne(row[0], [Token(row[i], row[i+1], row[i+2]) for i in range(1, len(row)-1, 3)], row[-1]) for row in reader]
 
 
 
 def clean_lines(liste_lignes: List[Ligne]) -> List[Ligne]:
     """Nettoie les lignes de la liste."""
     for ligne in liste_lignes:
-        ligne.texte = ligne.texte.replace("␣"," ")
-        ligne.texte = ligne.texte.replace("⌫","~")
+        ligne.texte_complete = ligne.texte_complete.replace("␣"," ")
     return liste_lignes
-
-
-file_list = get_filenames("TextesFinaux_txt")
-lexique = obtenir_lexique(file_list)
-liste_lignes = clean_lines(liste_lignes)
+            
 
 
 def compare_data_lexique(liste_lignes: List[Ligne], lexique: List[str]):
     """Compare les données avec le lexique et retourne une liste d'erreurs."""
     n = 0
     liste_erreurs = []
-    for ligne in tqdm(liste_lignes, desc="Détection des erreurs"):
-        mots_originaux = nlp(ligne.texte)
+    for ligne in liste_lignes:
+        mots_originaux = nlp(ligne.texte_complete)
         for mot in mots_originaux:
             if mot.text!=" " and mot.text.lower() not in lexique:
-                erreur = Erreur(str(mot), n, "Unknown", mot.pos_)
+                erreur = Difference(mot, n, "Unknown", mot.pos_)
                 liste_erreurs.append(erreur)
                 # print(f"{n}. Erreur : {mot}")
                 n += 1
     return liste_erreurs
 
-liste_erreurs = compare_data_lexique(liste_lignes, lexique)
+def main():
+    liste_lignes = ouverture_csv("csv_planification.csv")
+    file_list = get_filenames("TextesFinaux_txt")
+    lexique = obtenir_lexique(file_list)
+    liste_lignes = clean_lines(liste_lignes)
 
+    liste_erreurs = compare_data_lexique(liste_lignes, lexique)
 
-###############################################################################
+    print("Ligne\tMot erroné\tPos réel\tPos supposé\n")
+    for error in liste_erreurs:
 
-def annotate_corpus(corpus: List[Ligne]) -> List[Ligne] :
-    '''Ajoute les tokens à chaque ligne du corpus'''
-    corpus_annote = []
-    for ligne in tqdm(corpus, desc="Annotation du corpus") :
-        tokens = nlp(ligne.texte)
-        liste_tokens = []
-        for token in tokens :
-            token = Token(token.text, token.pos_, token.lemma_, "erreur")
-            liste_tokens.append(token)
-        ligne.tokens = liste_tokens
-        corpus_annote.append(ligne)
-    return(corpus_annote)
+        print(f"{error.ligne}\t{error.mot_errone}\t{error.pos_reel}\t{error.pos_suppose}")
 
-corpus_annote = annotate_corpus(liste_lignes)
-
-
-
-def extract_burst(file : str) -> List[str]:
-    '''Ouvre un fichier csv et retourne une liste de lignes.'''
-    with open (file, 'r', encoding='utf-8') as f:
-        reader = csv.reader(f, delimiter='\t') ## le delimiter de notre csv c'est des tabs
-        liste_lignes = []
-        for row in tqdm(reader, desc="Extraction du burst"):
-            line = str(row[11])
-            liste_lignes.append(line)
-        return liste_lignes
-
-burst = extract_burst('csv_planification.csv')
-
-
-
-def Ligne_burst(corpus: List[Ligne], burst: str) -> List[List] :
-    '''prend en entrée le corpus annoté et la liste de burst et ajoute le burst à chaque ligne'''
-    # Convertir chaque Ligne en liste
-    corpus_liste = []
-    for ligne in tqdm(corpus, desc="Ajout du burst à chaque ligne") :
-        ligne_liste = [ligne.texte, ligne.tokens, ligne.categorie]
-        corpus_liste.append(ligne_liste)
-    # Ajouter chaque burst à la ligne correspondante
-    corpus_avec_burst = []
-    for i in range(len(corpus_liste)) :
-        corpus_liste[i].append(burst[i])
-        corpus_avec_burst.append(corpus_liste[i])
-    return corpus_avec_burst
-
-corpus_burst = Ligne_burst(corpus_annote, burst)
-
-
-
-def mots_colles_burst(corpus_avec_burst: List[List]) -> List[List] :
-    '''prend en entrée le corpus de lignes sous forme de listes avec le burst et immite les mots_colles dans le burst'''
-    liste_burst = []
-    for ligne in tqdm(corpus_avec_burst, desc="Collage des mots dans le burst") :
-        burst = ligne[3]
-        liste_mots = burst.split()
-        coller_ligne = []
-        for i in range(len(liste_mots)-1) :
-            coller = liste_mots[i]+liste_mots[i+1]
-            coller_ligne.append(coller)
-        ligne[3] = coller_ligne
-        liste_burst.append(ligne)
-    return liste_burst
-
-#corpus_avec_burst_colles = mots_colles_burst(corpus_burst)
-
-
-
-def detecter_mots_colles(corpus_burst_colles: List[List]) -> List[Ligne] :
-    '''détecte les mots collés dans les lignes sous forme de listes et renvoie toutes les lignes avec l'erreur "mots collés" si détecté'''
-    corpus_erreur = []
-    for ligne in tqdm(corpus_burst_colles, desc="Détection des mots collés avec le mot suivant") :
-        for token in ligne[1] :
-            if token.texte in ligne[3] :
-                token.erreur = "mots collés"
-    for ligne_liste in corpus_burst_colles :
-        ligne = Ligne(ligne_liste[0], ligne_liste[1], ligne_liste[2])
-        corpus_erreur.append(ligne)
-    return corpus_erreur
-
-#corpus_mots_colles = detecter_mots_colles(corpus_avec_burst_colles)
-
-
-###############################################################################
-
-def mots_colles_premiere_lettre_burst(corpus_avec_burst: List[List]) -> List[List] :
-    '''prend en entrée le corpus de lignes sous forme de listes avec le burst et immite les mots_colles dans le burst'''
-    liste_burst = []
-    for ligne in tqdm(corpus_avec_burst, desc="Collage des mots avec la première lettre du mot suivant dans le burst") :
-        burst = ligne[3]
-        liste_mots = burst.split()
-        coller_ligne = []
-        for i in range(len(liste_mots)-1) :
-            coller = liste_mots[i]+liste_mots[i+1][0]
-            coller_ligne.append(coller)
-        ligne[3] = coller_ligne
-        liste_burst.append(ligne)
-    return liste_burst
-
-#corpus_avec_burst_colles_premiere_lettre = mots_colles_premiere_lettre_burst(corpus_burst)
-
-
-
-def detecter_mots_colles_premiere_lettre(corpus_burst_colles: List[List]) -> List[Ligne] :
-    '''détecte les mots collés dans les lignes sous forme de listes et renvoie toutes les lignes avec l'erreur "mots collés" si détecté'''
-    corpus_erreur = []
-    for ligne in tqdm(corpus_burst_colles, desc="Détection des mots collés avec la première lettre du mot suivant") :
-        for token in ligne[1] :
-            if token.texte in ligne[3] :
-                token.erreur = "mots collés à la première lettre du mot suivant"
-    for ligne_liste in corpus_burst_colles :
-        ligne = Ligne(ligne_liste[0], ligne_liste[1], ligne_liste[2])
-        corpus_erreur.append(ligne)
-    return corpus_erreur
-
-#corpus_mots_colles_premiere_lettre = detecter_mots_colles_premiere_lettre(corpus_avec_burst_colles_premiere_lettre)
+if __name__ == "__main__":
+    main()
